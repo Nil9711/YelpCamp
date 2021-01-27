705f4ab087a714d22594f2eba59c9c35301cc749
Nil Golan <Nil9711@icloud.com>
2021-01-27 13:29:30 +0200

    show route

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ app.get('/campgrounds', async (req, res) => {
     })
 })
 
+app.get('/campground/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {
+        campground
+    })
+})
+
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })

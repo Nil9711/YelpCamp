2e2581f2d44a5b88c607ef59f07b858ea23a5048
Nil Golan <Nil9711@icloud.com>
2021-01-31 11:17:20 +0200

    section 42

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -18,7 +20,7 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
@@ -26,6 +28,7 @@ app.use(express.urlencoded({
     extended: true
 }))
 app.use(methodOverride('_method'))
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
     res.render('home')

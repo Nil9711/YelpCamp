b1cb2ece9959b539ca3b57da2519f7ac43215689
Nil Golan <Nil9711@icloud.com>
2021-02-11 17:44:20 +0200

    section 55

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,71 +4,27 @@ const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/expressError')
 const Campground = require('../models/campground');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware')
+const campgrounds = require('../controllers/campgrounds')
+const multer = require('multer');
+const { storage } = require('../cloudinary')
+const upload = multer({ storage });
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampgrounds))
+
+// .post(upload.array('image'), (req, res) => {
+//     res.send('it works')
+// });
+
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 
-
-
-router.get('/', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {
-        campgrounds
-    })
-}));
-
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('campgrounds/new')
-});
-
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground');
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show', {
-        campground
-    })
-}));
-
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground');
-        return res.redirect('/campgrounds');
-    }
-
-    res.render('campgrounds/edit', {
-        campground
-    })
-
-}))
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(req.params.id, {
-        ...req.body.campground
-    });
-    req.flash('success', 'Successfully updated a campground')
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const campground = await Campground.findByIdAndDelete(req.params.id);
-    req.flash('success', 'successfully deleted a campground')
-    res.redirect(`/campgrounds`)
-}))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -29,7 +29,16 @@ const seedDB = async () => {
             author: '60213cc71c7cfc124c031d7b',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                    url: "https://res.cloudinary.com/db9dlwp6d/image/upload/v1613031334/YelpCamp/k8dtql5t6rmxbiv9vymk.jpg",
+                    filename: "YelpCamp/k8dtql5t6rmxbiv9vymk"
+                },
+                {
+                    url: "https://res.cloudinary.com/db9dlwp6d/image/upload/v1613031335/YelpCamp/dutljfst2tdigh3o4dgm.jpg",
+                    filename: "YelpCamp/dutljfst2tdigh3o4dgm"
+                }
+            ],
             description: `Lorem ipsum dolor sit
             amet consectetur adipisicing elit. Quisquam beatae provident rem, facere nostrum cum deleniti error aperiam
             soluta omnis dolorum earum, voluptate ducimus consequatur illo minima itaque. Commodi, voluptatibus!`,
